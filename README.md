594b6560e114114c965761120f882f80933c69e6
desyang <desyang@qq.com>
2026-06-30 16:11:57 +0800

    初始化：项目代码初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# ONNXRuntime infer
+
+使用ONNXRuntime cpp 进行模型推理
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <onnxruntime_cxx_api.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+    Ort::SessionOptions ops;
+
+    std::cout << "project init" << std::endl;
+    
+    return 0;
+}
