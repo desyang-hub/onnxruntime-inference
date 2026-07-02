a8b97154786d62ebc4d26a1969e45d2c7079cee3
desyang <desyang@qq.com>
2026-07-02 20:44:15 +0800

    修正：架构差异调整 - linux 下需要使用${OpenCV_LIBS} - 为copy_runtime_dlls设置为仅win环境下使用

--- cmake/copy_runtime_dlls.cmake
+++ cmake/copy_runtime_dlls.cmake
@@ -1,5 +1,6 @@
 # cmake/copy_runtime_dlls.cmake
 function(copy_runtime_dlls TARGET_NAME)
+if(WIN32)
     add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:${TARGET_NAME}>"
         COMMAND ${CMAKE_COMMAND} -E copy
@@ -8,4 +9,5 @@ function(copy_runtime_dlls TARGET_NAME)
         COMMAND_EXPAND_LISTS
         COMMENT "Copying runtime DLLs for ${TARGET_NAME}..."
     )
+endif()
 endfunction()
